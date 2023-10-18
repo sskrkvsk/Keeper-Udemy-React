539d10ee5879592dc470679db9c5c7738016aeb7
SS KR <s99ruy@gmail.com>
2023-10-18 10:18:08 +0200

    Start of the project. Just Header, footer and one Note. Files create

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+    return <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>
+}
+
+export default App;
