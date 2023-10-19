0475d5bbc154fc1dfa3ccebf42bb07404508d29a
SS KR <s99ruy@gmail.com>
2023-10-19 10:24:04 +0200

    add Note file with more notes. Shorten the app.js with arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+
+
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(note => (<Note key={note.key} title={note.title} content={note.content} />))}
         <Footer />
     </div>
 }
